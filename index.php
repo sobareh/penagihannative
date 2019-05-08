114b6f753a858f704d67e7e655138c1c2b4e22bc
ACHMAD SOBARI <910222897@intranet.pajak.go.id>
2019-05-08 17:33:01 +0700

    Inisiasi Halaman login dengan bootstrap dan prepare datatables

--- index.php
+++ index.php
@@ -1,66 +1,109 @@
 <?php 
     session_start();
-
+    
     if( !isset($_SESSION["login"]) ) {
         header("Location: login.php");
         exit;
     }
 
-    require 'functions.php';
-    $data = query("SELECT * FROM komputer");
+    //require 'functions.php';
+    //$data = query("SELECT * FROM komputer");
 
     //tombol cari ditekan
     if( isset($_POST["cari"]) ) {
         $data = cari($_POST["keyword"]);
     }
-
 ?>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Aplikasi Penagihan</title>
+    <title>Halaman </title>
+    <link rel="stylesheet" href="http://localhost/penagihannative/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="http://localhost/penagihannative/datatables/datatables.min.css" />
 </head>
+
 <body>
-    <a href="logout.php">Logout</a>
-    <h1>Data Komputer</h1>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="">SMART</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link active" href="">Home</a>
+                    <a class="nav-item nav-link" href="">Data RBK</a>
+                    <a class="nav-item nav-link" href="">About</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <div class="container">
+        <a href="logout.php">Logout</a>
+        <h1>Data Komputer</h1>
 
-    <a href="tambah.php">Tambah Data Komputer</a> 
-    <br><br>
+        <a href="tambah.php">Tambah Data Komputer</a>
+        <br><br>
 
-    <form action="" method="post">
-        <input type="text" name="keyword" size="30" autofocus placeholder="masukkan keyword pencarian..." autocomplete="off">
-        <button type="submit" name="cari">Cari!</button>
-    </form>
-
-    <table border="1" cellpadding="10" cellspacing="0">
-        <tr>
-            <th>No.</th>
-            <th>Gambar</th>
-            <th>Serial Number</th>
-            <th>Merek Komputer</th>
-            <th>Model Komputer</th>
-            <th>Email</th>
-            <th>Aksi</th>
-        </tr>
-        <?php $i = 1 ; ?>
-        <?php foreach( $data as $row ) :?>
-        <tr>
-            <td><?= $i; ?></td>
-            <td><img src="img/<?= $row["gambar"]; ?>" width="50"></td>
-            <td><?= $row["sn"]; ?></td>
-            <td><?= $row["merek"]; ?></td>
-            <td><?= $row["model"]; ?></td>
-            <td><?= $row["email"]; ?></td>
-            <td>
-                <a href="detail.php?id=<?= $row["id"];?>" >Detail</a> |
-                <a href="ubah.php?id=<?= $row["id"];?>" >Ubah</a> |   
-                <a href="hapus.php?id=<?= $row["id"]; ?>" onclick="return confirm('Yakin Hapus Data?');">Hapus</a>
-            </td>
-        </tr>
-            <?php $i++; ?>
-        <?php endforeach; ?>
-    </table>
+        <form action="" method="post">
+            <input type="text" name="keyword" size="30" autofocus placeholder="masukkan keyword pencarian..."
+                autocomplete="off">
+            <button type="submit" name="cari">Cari!</button>
+        </form>
+        <br>
+        <table class="table table-bordered datatabel">
+            <thead class="thead-dark">
+                <tr align="center">
+                    <th>No.</th>
+                    <th>Serial Number</th>
+                    <th>Merek Komputer</th>
+                    <th>Model Komputer</th>
+                    <th>Email</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php 
+            require 'functions.php';
+            $data = query("SELECT * FROM komputer");
+            $i = 1 ;
+            foreach( $data as $row ) {
+            ?>
+                <tr>
+                        <td><?= $i++; ?></td>
+                        <td><?= $row["sn"]; ?></td>
+                        <td><?= $row["merek"]; ?></td>
+                        <td><?= $row["model"]; ?></td>
+                        <td><?= $row["email"]; ?></td>
+                        <td>
+                            <a href="detail.php?id=<?= $row["id"];?>">Detail</a> |
+                            <a href="ubah.php?id=<?= $row["id"];?>">Ubah</a> |
+                            <a href="hapus.php?id=<?= $row["id"]; ?>"
+                                onclick="return confirm('Yakin Hapus Data?');">Hapus</a>
+                            <a class="btn btn-sm btn-warning" href="file/<?= $row['gambar']; ?>">Download</a>
+                        </td>
+                    </tr>
+                 
+             <?php
+              
+                }
+             ?>
+            </tbody>
+        </table>
+    </div>
+    <script type="text/javascript" src="C:\xampp\htdocs\penagihannative\jquery\src\jquery.js"></script>
+    <script type="text/javascript" src="http://localhost/penagihannative/js/bootstrap.js"></script>
+    <script type="text/javascript" src="http://localhost/penagihannative/datatables/datatables.min.js"></script>
+    <script>
+        $(document).ready(function () {
+            $('datatabel').DataTable();
+        });
+    </script>
 </body>
+
 </html>
--- login.php
+++ login.php
@@ -1,19 +1,18 @@
 <?php
-
     session_start();
-    
     if( isset($_SESSION["login"]) ) {
         header("Location: index.php");
         exit;
     }
 
-    require 'functions.php';
+    include 'functions.php';
+   
     if( isset($_POST["login"]) ){
 
         $username = $_POST["username"];
         $password = $_POST["password"];
 
-        $result = mysqli_query($conn, "SELECT * FROM user WHERE username = '$username' ");
+        $result = mysqli_query($conn, "SELECT * FROM user WHERE username = '$username'");
 
         //check username
         if( mysqli_num_rows($result) === 1 ) {
@@ -25,46 +24,135 @@
                 //cek session
                 $_SESSION["login"] = true ;
 
-                header("Location: index.php");
+                header("location:index.php");
                 exit;
             }
         }
 
-        $error = true;
+       
     }
-
+    
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Halaman Login</title>
+    <title>SMART - Login</title>
+    <link rel="stylesheet" href="http://localhost/penagihannative/css/bootstrap.css">
+    <style>
+ .body 
+ {
+    background-color: #FEFDFD;
+ }
+ .form-signin
+ {
+     max-width: 330px;
+     padding: 15px;
+     margin: 0 auto;
+ }
+ .form-signin .form-signin-heading, .form-signin .checkbox
+ {
+     margin-bottom: 10px;
+ }
+ .form-signin .checkbox
+ {
+     font-weight: normal;
+ }
+ .form-signin .form-control
+ {
+     position: relative;
+     font-size: 16px;
+     height: auto;
+     padding: 10px;
+     -webkit-box-sizing: border-box;
+     -moz-box-sizing: border-box;
+     box-sizing: border-box;
+ }
+ .form-signin .form-control:focus
+ {
+     z-index: 2;
+ }
+ .form-signin input[type="text"]
+ {
+     margin-bottom: -1px;
+     border-bottom-left-radius: 0;
+     border-bottom-right-radius: 0;
+ }
+ .form-signin input[type="password"]
+ {
+     margin-bottom: 10px;
+     border-top-left-radius: 0;
+     border-top-right-radius: 0;
+ }
+ .account-wall
+ {
+     margin-top: 20px;
+     padding: 40px 0px 20px 0px;
+     background-color: #f7f7f7;
+     -moz-box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+     -webkit-box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+     box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+ }
+ .login-title
+ {
+     color: #555;
+     font-size: 40px;
+     font-weight: 400;
+     display: block;
+ } 
+ .profile-img
+ {
+     width: 96px;
+     height: 96px;
+     margin: 0 auto 10px;
+     display: block;
+     -moz-border-radius: 50%;
+     -webkit-border-radius: 50%;
+     border-radius: 50%;
+ }
+ .need-help
+ {
+     margin-top: 10px;
+ }
+ .new-account
+ {
+     display: block;
+     margin-top: 10px;
+ }
+ </style>
 </head>
-<body>
-    <h1>Halaman Login</h1>
-    <?php if( isset($error) ) : ?>
-        <p style="color: red; font-style: italic;">username / password salah</p>
-<?php endif; ?>
+<body class="body">
+<?php
+    
 
-    <form action="" method="post">
-
-        <ul>
-            <li>
-                <label for="username">Username :</label>
-                <input type="text" name="username" id="username">
-            </li>
-            <li>
-                <label for="password">Password : </label>
-                <input type="text" name="password" id="password">
-            </li>
-            <li>
-                <button type="submit" name="login">Login</button>
-            </li>
-        </ul>
-
-    </form>
+?>
+<div class="container text-center">
+<div class="row">
+    <div class="col-sm-6 col-md-4 col-md-offset-4 mt-5 mx-auto">
+        <br><br>
+    <!-- <h1 class="text-center login-title">Login</h1> -->
+        <div class="account-wall">
+        <h1 class="text-center mx-auto login-title"><img src="http://localhost/penagihannative/img/stumbleupon.png " class="ui image" width="80">&nbsp;Login</h1>
+            <!-- <img class="profile-img" src="http://localhost/penagihannative/img/stumbleupon.png"
+                alt=""> -->
+            <form class="form-signin" action="login.php" method="post">
+            <i class="fa fa-envelope"></i>
+            <input type="text" class="form-control" placeholder="username" name="username" id="username" required autofocus>
+            <input type="password" class="form-control" placeholder="password" name="password" id="password" required>
+            <br>
+            <button class="btn btn-lg btn-dark btn-block" type="submit" name="login" id="login">
+                Login</button>
+                <br><br>
+            </form>
+            <!-- <p>&copy; KPP Pratama Medan Barat 2019</p> -->
+        </div>
+        
+    </div>
+</div>
+</div>
 </body>
 </html>
+
+
